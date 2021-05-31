a42661578889dba5135232ebd2cdf4b9b8004b27
Ruchita <Ruchita@DESKTOP-GI2UJ9Q>
2021-05-31 21:28:55 +0530

    Changed display based on brand

--- src/product_package/ProductImplementation.java
+++ src/product_package/ProductImplementation.java
@@ -2,6 +2,7 @@
 package product_package;
 import linkedlist_package.*;
 
+
 import java.util.Scanner;
 
 import java.sql.*;
@@ -344,6 +345,7 @@ public class ProductImplementation
 			System.out.println("Enter the product brand that has to be searched");
 			brand = sc.next();
 			Node temp = ll.getHead();
+			
 			while(temp!=null)  //traverse till the end
 			{
 				if((((Product)temp.getData()).getBrand()).equalsIgnoreCase(brand))
@@ -354,16 +356,16 @@ public class ProductImplementation
 						System.out.printf("\n|%-15s|%-15s|%-15s|%-20s|%-15s|%-15s|%-15s|", " "," "," "," "," "," "," ");
 						flag = 1;
 					}
-					System.out.println("Product found. Details are: ");
 					this.displayProductDetails((Product)temp.getData());
 					result = true;//product found
+					
 				}
 				temp = temp.getNext();
 			}
 		}
 		if(result==false)
 		{
-			System.out.println("The product could not be found");
+			System.out.println("\nThe product could not be found");
 		}
 
 	}
